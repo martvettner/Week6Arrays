e02d9c4df6efd485c17cea67eb92c98b5ceb8776
mart vettner <martvettner@gmail.com>
2020-10-05 14:24:09 +0300

    Add project files.

---
+++ WardobePicker/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WardrobePicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wardrobe = { "hat", "hoodie", "jeans", "shirt", "underwear" };
+            string[] colors = { "red", "white", "purple", "blue" };
+            string[] patterns = { "flowery", "striped", "circles", "checkered" };
+
+            Console.WriteLine("Do you need help picking an outfit?");
+            string userInput = Console.ReadLine().ToLower();
+            if (userInput == "no")
+            {
+                Console.WriteLine("Farewell.");
+            }
+            else
+            {
+                Random rnd = new Random();
+                int randomWardrobe = rnd.Next(1, wardrobe.Length);
+                int randomColors = rnd.Next(1, colors.Length);
+                int randomPatterns = rnd.Next(1, patterns.Length);
+                Console.WriteLine($"Today you should wear {colors[randomColors]} {patterns[randomPatterns]} {wardrobe[randomWardrobe]}.");
+            }
+
+        }
+    }
+}
---
+++ Week6Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Week6Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] predictions = { "win million", "lose your smartphone", "fall in love", "buy new pc", "move out of my parents house" };
+            //today you will...
+            Random rnd = new Random();
+            int randomindex = rnd.Next(0, predictions.Length);
+            Console.WriteLine($"today you will {predictions[randomindex]}");
+
+
+
+
+
+        }
+    }
+}
